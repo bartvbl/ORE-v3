0dc3f3b7e382d7ea2c69b25bc526adb3c3367459
bartvbl <bartvblokl@gmail.com>
2021-10-21 12:59:57 +0200

    compilation fix

--- src/ore/geom/RelativeTransformation.h
+++ src/ore/geom/RelativeTransformation.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "vec3.h"
 
 namespace ore {
     struct RelativeTransformation {
-        ore::vec3 position = {0, 0, 0};
-        ore::vec3 rotation = {0, 0, 0};
+        ore::geom::vec3 position = {0, 0, 0};
+        ore::geom::vec3 rotation = {0, 0, 0};
         float scale = 1;
     };
 }
