23b191e35df2d0777fc8b3a7514f41190e3815c5
Bart van Blokland <bartvblokl@gmail.com>
2021-10-23 02:21:56 +0200

    Poor man's render pass

--- src/ore/gl/render/RenderPass.cpp
+++ src/ore/gl/render/RenderPass.cpp
@@ -2,5 +2,10 @@
 #include "RenderPass.h"
 
 void ore::RenderPass::render(ore::SceneNode *rootNode) {
-
+    ore::RenderState state;
+    rootNode->preUpdate(state);
+    rootNode->preRender(state);
+    rootNode->render(state);
+    rootNode->postRender(state);
+    rootNode->postUpdate(state);
 }
