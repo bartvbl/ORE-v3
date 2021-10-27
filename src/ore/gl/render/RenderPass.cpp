3351109773c687b6e17ef876f9bceb334ad26b51
bartvbl <bartvblokl@gmail.com>
2021-10-27 02:10:49 +0200

    Actual implementation of render pass

--- src/ore/gl/render/RenderPass.cpp
+++ src/ore/gl/render/RenderPass.cpp
@@ -1,11 +1,25 @@
 #include <glad/glad.h>
 #include "RenderPass.h"
 
+void doRenderPassUpdate(ore::SceneNode* node, ore::RenderState &renderState) {
+    node->preUpdate(renderState);
+    for(ore::SceneNode* childNode : *node->getChildren()) {
+        doRenderPassUpdate(childNode, renderState);
+    }
+    node->postUpdate(renderState);
+}
+
+void doRenderPassRender(ore::SceneNode* node, ore::RenderState &renderState) {
+    node->preRender(renderState);
+    node->render(renderState);
+    for(ore::SceneNode* childNode : *node->getChildren()) {
+        doRenderPassRender(childNode, renderState);
+    }
+    node->postRender(renderState);
+}
+
 void ore::RenderPass::render(ore::SceneNode *rootNode) {
     ore::RenderState state;
-    rootNode->preUpdate(state);
-    rootNode->preRender(state);
-    rootNode->render(state);
-    rootNode->postRender(state);
-    rootNode->postUpdate(state);
+    doRenderPassUpdate(rootNode, state);
+    doRenderPassRender(rootNode, state);
 }
