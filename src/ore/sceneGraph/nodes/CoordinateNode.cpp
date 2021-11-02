5b59df6b93c5cb207682823280e5001ad3c0bfbc
bartvbl <bartvblokl@gmail.com>
2021-11-02 02:22:48 +0100

    Fixed transformations not using radians

--- src/ore/sceneGraph/nodes/CoordinateNode.cpp
+++ src/ore/sceneGraph/nodes/CoordinateNode.cpp
@@ -14,9 +14,9 @@ void ore::scene::CoordinateNode::preRender(ore::RenderState &renderState) {
     glm::mat4 relativeTransformation(1.0);
     relativeTransformation = glm::translate(relativeTransformation, position);
     relativeTransformation = glm::translate(relativeTransformation, pivot);
-    relativeTransformation = glm::rotate(relativeTransformation, rotation.x, glm::vec3(1, 0, 0));
-    relativeTransformation = glm::rotate(relativeTransformation, rotation.y, glm::vec3(0, 1, 0));
-    relativeTransformation = glm::rotate(relativeTransformation, rotation.z, glm::vec3(0, 0, 1));
+    relativeTransformation = glm::rotate(relativeTransformation, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+    relativeTransformation = glm::rotate(relativeTransformation, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+    relativeTransformation = glm::rotate(relativeTransformation, glm::radians(rotation.z), glm::vec3(0, 0, 1));
     relativeTransformation = glm::scale(relativeTransformation, scale);
     relativeTransformation = glm::translate(relativeTransformation, -pivot);
 
--- src/ore/sceneGraph/nodes/PerspectiveCamera.cpp
+++ src/ore/sceneGraph/nodes/PerspectiveCamera.cpp
@@ -2,6 +2,7 @@
 #include "PerspectiveCamera.h"
 
 void ore::scene::PerspectiveCamera::render(ore::RenderState &renderState) {
+    CoordinateNode::preRender(renderState);
     glm::mat4 previousViewMatrix = renderState.transformations.view;
     glm::mat4 previousProjectionMatrix = renderState.transformations.projection;
 
@@ -12,18 +13,17 @@ void ore::scene::PerspectiveCamera::render(ore::RenderState &renderState) {
     glm::mat4 relativeMatrix = glm::inverse(renderState.transformations.model);
 
     glm::mat4 positionTransformation(1.0);
-    positionTransformation = glm::rotate(positionTransformation, rotation.z, glm::vec3(0, 0, 1));
-    positionTransformation = glm::rotate(positionTransformation, rotation.x, glm::vec3(1, 0, 0));
-    positionTransformation = glm::rotate(positionTransformation, rotation.y, glm::vec3(0, 1, 0));
+    positionTransformation = glm::rotate(positionTransformation, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+    positionTransformation = glm::rotate(positionTransformation, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+    positionTransformation = glm::rotate(positionTransformation, glm::radians(rotation.y), glm::vec3(0, 1, 0));
     positionTransformation = glm::translate(positionTransformation, position);
-
-
-
 
     renderState.transformations.view = relativeMatrix * positionTransformation;
 
-    SceneNode::render(renderState);
+    CoordinateNode::render(renderState);
 
     renderState.transformations.view = previousViewMatrix;
     renderState.transformations.projection = previousProjectionMatrix;
+
+    CoordinateNode::postRender(renderState);
 }
