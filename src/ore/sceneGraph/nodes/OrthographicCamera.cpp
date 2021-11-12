f2997c6a5a8b1b10fae0902aad69cb7375d76bd1
Bart van Blokland <bartvblokl@gmail.com>
2021-11-12 14:48:04 +0100

    Fixed orthographic camera not restoring the projection matrix

--- src/ore/sceneGraph/nodes/OrthographicCamera.cpp
+++ src/ore/sceneGraph/nodes/OrthographicCamera.cpp
@@ -2,9 +2,11 @@
 #include "OrthographicCamera.h"
 
 void ore::scene::OrthographicCamera::render(ore::RenderState &renderState) {
+    glm::mat4 currentProjectionMatrix = renderState.transformations.projection;
     renderState.transformations.projection = glm::ortho(left, right, bottom, top, zNear, zFar);
 
     ContainerNode::render(renderState);
+    renderState.transformations.projection = currentProjectionMatrix;
 }
 
 void ore::scene::OrthographicCamera::setBounds(float _left, float _right, float _bottom, float _top, float _near, float _far) {
