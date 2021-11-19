00c6b79f8e8fd0e45256e6893fa79c20a49dff4f
bartvbl <bartvblokl@gmail.com>
2021-11-19 18:48:40 +0100

    Fixed material node overwriting render state

--- src/ore/sceneGraph/nodes/MaterialNode.cpp
+++ src/ore/sceneGraph/nodes/MaterialNode.cpp
@@ -2,6 +2,8 @@
 #include <ore/gl/shader/ShaderUniformIndex.h>
 
 void ore::scene::MaterialNode::render(ore::RenderState &state) {
+    ore::gl::ShadingConfiguration oldConfig = state.shading;
+
     if(material->hasDiffuseTexture() && state.shading.enableTexturing) {
         state.uniforms.setTexture(ore::gl::ShaderUniformIndex::diffuseTextureID, material->diffuseTexture);
     } else {
@@ -11,4 +13,6 @@ void ore::scene::MaterialNode::render(ore::RenderState &state) {
     state.uniforms.setDiffuseColour(ore::gl::ShaderUniformIndex::materialColourDiffuse, glm::vec4(material->diffuseColour.x, material->diffuseColour.y, material->diffuseColour.z, material->diffuseColour.w));
 
     ContainerNode::render(state);
+
+    state.shading = oldConfig;
 }
