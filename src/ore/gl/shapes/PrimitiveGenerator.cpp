598cfe3dfffd6fb65880e812a40f311a9d65b91c
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2025-04-25 15:45:46 +0200

    Added circle generator

--- src/ore/gl/shapes/PrimitiveGenerator.cpp
+++ src/ore/gl/shapes/PrimitiveGenerator.cpp
@@ -20,3 +20,35 @@ ore::scene::GeometryNode ore::gl::PrimitiveGenerator::generatePlaneBuffer(glm::v
     ore::gl::GeometryBuffer planeBuffer = ore::gl::generateGeometryBuffer(geometry);
     return {planeBuffer};
 }
+
+ore::scene::GeometryNode ore::gl::PrimitiveGenerator::generateCircleBuffer(glm::vec2 center, float radius, uint32_t sliceCount) {
+    ore::resources::MeshGeometry geometry;
+
+    geometry.vertices.resize(sliceCount + 1);
+    geometry.indices.resize(3 * sliceCount);
+    geometry.hasTextures = false;
+    geometry.hasNormals = false;
+    geometry.hasNormalMap = false;
+    geometry.vertices.at(0) = glm::vec3(center, 0);
+
+    for (uint32_t i = 1; i <= sliceCount; i++) {
+        glm::vec2 outerVertex;
+        float fraction = float(i) / float(sliceCount);
+        float angle = fraction * (M_PI * 2.0f);
+        geometry.vertices.at(i) = glm::vec3(center.x + (radius * float(std::cos(angle))),
+                                center.y + (radius * float(std::sin(angle))), 0.0f);
+
+        // Main circle index buffer
+        geometry.indices.at(3 * (i - 1) + 0) = 0;
+        geometry.indices.at(3 * (i - 1) + 1) = i - 1;
+        geometry.indices.at(3 * (i - 1) + 2) = i;
+    }
+
+    // Correct the first triangle which connects the first and last vertex
+    geometry.indices.at(0) = 0;
+    geometry.indices.at(1) = sliceCount;
+    geometry.indices.at(2) = 1;
+
+    return ore::gl::generateGeometryBuffer(geometry);
+}
+
--- src/ore/gl/shapes/PrimitiveGenerator.h
+++ src/ore/gl/shapes/PrimitiveGenerator.h
@@ -5,6 +5,7 @@ namespace ore {
     namespace gl {
         namespace PrimitiveGenerator {
             ore::scene::GeometryNode generatePlaneBuffer(glm::vec2 bottomLeft = {0, 0}, glm::vec2 topRight = {1, 1});
+            ore::scene::GeometryNode generateCircleBuffer(glm::vec2 center = {0, 0}, float radius = 1, uint32_t sliceCount = 45);
         };
     }
 }
