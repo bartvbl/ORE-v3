4769bc6456f8bdb6b117137dc8897da6546415b1
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2025-08-23 14:04:00 +0200

    Try to support norwegian characters

--- src/ore/resources/resourceTypes/FontResource.cpp
+++ src/ore/resources/resourceTypes/FontResource.cpp
@@ -7,8 +7,11 @@
 #include FT_FREETYPE_H
 
 const unsigned int CHARACTER_PADDING_PIXELS = 2;
-const std::array<unsigned int, 7> CHARACTER_SIZES = {8, 9, 10, 11, 12, 18, 48};
+const std::array<unsigned int, 9> CHARACTER_SIZES = {8, 9, 10, 11, 12, 18, 32, 48, 96};
 const unsigned int CHAR_COUNT_TO_LOAD = 128;
+const std::array<unsigned int, 6> specialCharacters = {
+        0xE5, 0xE6, 0xF8, 0xD8, 0xC5, 0xC6
+};
 
 void computeTextureDimensions(FT_Face &face, unsigned int &textureWidth, unsigned int &textureHeight, const ore::filesystem::path &fileLocation) {
     textureWidth = 0;
@@ -20,6 +23,18 @@ void computeTextureDimensions(FT_Face &face, unsigned int &textureWidth, unsigne
             LOG(FATAL) << "Failed to load font from " + fileLocation.string() + ": Could not create the character '" +
                           (char) c + "'" << std::endl;
         }
+
+        textureWidth += face->glyph->bitmap.width + CHARACTER_PADDING_PIXELS;
+        textureHeight = std::max<unsigned int>(textureHeight, face->glyph->bitmap.rows);
+    }
+
+    for (unsigned int specialCharacter : specialCharacters) {
+        // Load character glyph
+        if (FT_Load_Char(face, specialCharacter, FT_LOAD_RENDER)) {
+            LOG(FATAL) << "Failed to load font from " + fileLocation.string() + ": Could not create the character '" <<
+                        specialCharacter << "'" << std::endl;
+        }
+
         textureWidth += face->glyph->bitmap.width + CHARACTER_PADDING_PIXELS;
         textureHeight = std::max<unsigned int>(textureHeight, face->glyph->bitmap.rows);
     }
@@ -103,6 +118,47 @@ void ore::resources::FontResource::load(const ore::filesystem::path &fileLocatio
 
             baseXCoordinate += face->glyph->bitmap.width + CHARACTER_PADDING_PIXELS;
         }
+
+        for (unsigned int specialCharacter : specialCharacters) {
+            // Load character glyph
+            if (FT_Load_Char(face, specialCharacter, FT_LOAD_RENDER)) {
+                LOG(FATAL) << "Failed to load font from " + fileLocation.string() + ": Could not create the character '" <<
+                           specialCharacter << "'" << std::endl;
+            }
+
+            for(unsigned int y = 0; y < face->glyph->bitmap.rows; y++) {
+                for(unsigned int x = 0; x < face->glyph->bitmap.width; x++) {
+                    unsigned char pixelValue = face->glyph->bitmap.buffer[(y * face->glyph->bitmap.width) + x];
+
+                    unsigned int pixelXCoordinate = baseXCoordinate + x;
+                    unsigned int pixelYCoordinate = baseYCoordinate + face->glyph->bitmap.rows - 1 - y;
+                    unsigned int pixelIndex = totalTextureWidth * pixelYCoordinate + pixelXCoordinate;
+                    unsigned int arrayIndex = sizeof(unsigned int) * pixelIndex;
+
+                    // Set alpha channel
+                    characterTextureDataRGBA.at(arrayIndex + 0) = pixelValue;
+                    characterTextureDataRGBA.at(arrayIndex + 1) = pixelValue;
+                    characterTextureDataRGBA.at(arrayIndex + 2) = pixelValue;
+                    characterTextureDataRGBA.at(arrayIndex + 3) = pixelValue;
+                }
+            }
+
+            ore::gl::FontCharacter character;
+            character.textureCoordinatesBottomLeft = glm::vec2(float(baseXCoordinate) / float(totalTextureWidth),
+                                                               float(baseYCoordinate) / float(totalTextureHeight));
+            character.textureCoordinatesTopRight = glm::vec2(float(baseXCoordinate + face->glyph->bitmap.width) / float(totalTextureWidth),
+                                                             float(baseYCoordinate + face->glyph->bitmap.rows) / float(totalTextureHeight));
+            character.bearingPixels = glm::vec2(float(face->glyph->bitmap_left),float(face->glyph->bitmap_top));
+            character.sizePixels = glm::vec2(face->glyph->bitmap.width, face->glyph->bitmap.rows);
+            // Advance is given in 1/64th pixel, so we scale it to be measured in pixels.
+            character.advancePixels = float(static_cast<unsigned int>(face->glyph->advance.x)) / 64.0f;
+            characterMap[fontSize].push_back(character);
+
+            baseXCoordinate += face->glyph->bitmap.width + CHARACTER_PADDING_PIXELS;
+        }
+
+
+
         baseYCoordinate += rowHeights.at(rowIndex) + CHARACTER_PADDING_PIXELS;
         rowIndex++;
     }
