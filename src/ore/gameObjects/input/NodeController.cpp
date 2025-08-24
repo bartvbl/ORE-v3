7474cbb32f9c4ead1e2fbc5edb63e9212ad908b9
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2025-08-25 01:46:57 +0200

    Formatted output for coordinate dump

--- src/ore/gameObjects/input/NodeController.cpp
+++ src/ore/gameObjects/input/NodeController.cpp
@@ -1,5 +1,6 @@
 #include "NodeController.h"
 #include <iostream>
+#include <json.hpp>
 
 const float rotationSpeed = 2.5f;
 const float cameraSpeed = 0.06f;
@@ -32,7 +33,10 @@ void ore::NodeController::update() {
     node->position.y += (movementDown - movementUp) * cameraSpeed;
 
     if(printCameraOrientation) {
-        std::cout << "position: " << node->position.x << ", " << node->position.y << ", " << node->position.z << " - orientation: " << node->rotation.x << ", " << node->rotation.y << ", " << node->rotation.z << std::endl;
+        nlohmann::json outObject;
+        outObject["position"] = {node->position.x, node->position.y, node->position.z};
+        outObject["orientation"] = {node->rotation.x, node->rotation.y, node->rotation.z};
+        std::cout << outObject.dump(4) << std::endl;
     }
 
 }
